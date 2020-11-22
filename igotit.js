9f7e2c7ec764dd0577cff18f40499f194d3117cf
MrFIVEfourONE <choffman04@gmail.com>
2020-11-21 23:43:23 -0800

    got the prompts to work with the arrays i think

--- igotit.js
+++ igotit.js
@@ -15,14 +15,29 @@ var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-function generatePassword(){
+function generatePassword() {
     var passwordLength = parseInt(prompt('How long would you like the password to be(8 < x < 128)?'));
     var password = "";
     var charPool = [];
-    var includeSpecialChars = confirm("do you want password to include special chars?")
 
-    if (includeSpecialChars){
+    var includeSpecialChars = confirm("do you want password to include special chars?")
+    if (includeSpecialChars) {
         charPool = charPool.concat(specialChar);
+    }
+
+    var alphaUpper = confirm("do you want password to include Upper case?")
+    if (alphaUpper) {
+        charPool = charPool.concat(alphaUpper);
+    }
+
+    var alphaLower = confirm("do you want password to include Lower Case?")
+    if (alphaLower) {
+        charPool = charPool.concat(alphaLower);
+    }
+
+    var number = confirm("do you want password to include any Numbers?")
+    if (number) {
+        charPool = charPool.concat(number);
     }
 
     for (let i = 0; i < password.length; i++) {
