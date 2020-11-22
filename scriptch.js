ed6e5a5bc0c5d0c4daf3f72e2e752b6ec774d418
MrFIVEfourONE <choffman04@gmail.com>
2020-11-21 20:02:41 -0800

    found some code online that makes it work, im implementing it, now trying to reverse engineer it so i can make sence of the solutiuon. i have no other option at the moment

--- scriptch.js
+++ scriptch.js
@@ -2,12 +2,31 @@ var generateBtn = document.querySelector("#generate");
 
 var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
 
+
 var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
 
 
 function generatePassword() {
   
-
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+   
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
 
 generateBtn.addEventListener("click", writePassword);
 
