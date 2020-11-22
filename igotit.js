cac1640be955844637be49066776ed23bea92524
MrFIVEfourONE <choffman04@gmail.com>
2020-11-22 00:25:54 -0800

    i finaly made it workgit add -Agit add -A! with arrays, and being concated, and got fixed the () to [] at the end of my loop and now it post the password

--- igotit.js
+++ igotit.js
@@ -1,4 +1,8 @@
 var generateBtn = document.querySelector("#generate");
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function writePassword() {
     var password = generatePassword();
@@ -8,41 +12,36 @@ function writePassword() {
 
 }
 
-generateBtn.addEventListener("click", writePassword);
-
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
 function generatePassword() {
-    var passwordLength = parseInt(prompt('How long would you like the password to be(8 < x < 128)?'));
+    var passwordLength = parseInt(prompt('How long would you like the password to be?'));
     var password = "";
     var charPool = [];
 
-    var includeSpecialChars = confirm("do you want password to include special chars?")
-    if (includeSpecialChars) {
+    var includeSpecialChar = confirm("do you want password to include special chars?")
+    if (includeSpecialChar) {
         charPool = charPool.concat(specialChar);
     }
 
-    var alphaUpper = confirm("do you want password to include Upper case?")
-    if (alphaUpper) {
+    var includeAlphaUpper = confirm("do you want password to include Upper case?")
+    if (includeAlphaUpper) {
         charPool = charPool.concat(alphaUpper);
     }
 
-    var alphaLower = confirm("do you want password to include Lower Case?")
-    if (alphaLower) {
+    var includeAlphaLower = confirm("do you want password to include Lower Case?")
+    if (includeAlphaLower) {
         charPool = charPool.concat(alphaLower);
     }
 
-    var number = confirm("do you want password to include any Numbers?")
-    if (number) {
+    var includeNumber = confirm("do you want password to include any Numbers?")
+    if (includeNumber) {
         charPool = charPool.concat(number);
     }
 
-    for (let i = 0; i < password.length; i++) {
+    for (let i = 0; i < passwordLength; i++) {
         var randomNum = Math.floor(Math.random() * charPool.length);
-        password = password + charPool(randomNum);
+        password = password + charPool[randomNum];
     }
     return password;
 }
+
+generateBtn.addEventListener("click", writePassword);
