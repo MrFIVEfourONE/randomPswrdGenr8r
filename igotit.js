e68b1ff5adfbb2159fd1a73f7d0750916abce0ab
MrFIVEfourONE <choffman04@gmail.com>
2020-11-30 06:02:25 -0800

    added lots of CSS, created a working copy button in JS, fixed 3 bugs in my JS, added some html elemnts to round out the page, and just generaly polished it

--- igotit.js
+++ igotit.js
@@ -1,4 +1,9 @@
 var generateBtn = document.querySelector("#generate");
+// var selectBtn = document.querySelector("#selectBtn");
+var copyBtn = document.querySelector("#copyBtn")
+
+
+
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
@@ -12,34 +17,57 @@ function writePassword() {
 
 }
 
+function copyPassword() {
+    var copyText = document.querySelector("#password");
+    copyText.select();
+    document.execCommand("copy");
+  }
+  
+//   document.querySelector("#copyBtn").addEventListener("click", copyPassword);
+copyBtn.addEventListener("click", copyPassword);
+
+
 function generatePassword() {
-    var passwordLength = parseInt(prompt('How long would you like the password to be?'));
+    var passwordLength = parseInt(prompt('How many characters would you like your PASSWORD to be? \n It must be atleast 8 but no more then 128.'));
+    // added this conditional gate to validate the input
+    if (passwordLength < 8 || passwordLength > 128) {
+        return alert("oops! pick a number between 8-128")
+    }
+
+    var includeSpecialChar = confirm("Do you want your AWSOME password to include \n Special Characters?")
+    var includeAlphaUpper = confirm("How about \n Upper Case Characters?")
+    var includeAlphaLower = confirm("Or even some \n Lower Case Characters?")
+    var includeNumber = confirm("And lastly how about some \n Numbers?")
+    // added another conditional gate to validate this input as well, just incase the user selected 'cancel' on all 4 prompts
+    if (
+        !includeAlphaLower &&
+        !includeAlphaUpper &&
+        !includeNumber &&
+        !includeSpecialChar
+    ) {
+        return alert("!!MUST SELECT ATLEAST \n 1oneONE!\n TYPE OF CHARACTER!!")
+    }
     var password = "";
     var charPool = [];
 
-    var includeSpecialChar = confirm("do you want password to include special chars?")
     if (includeSpecialChar) {
         charPool = charPool.concat(specialChar);
     }
-
-    var includeAlphaUpper = confirm("do you want password to include Upper case?")
     if (includeAlphaUpper) {
         charPool = charPool.concat(alphaUpper);
     }
-
-    var includeAlphaLower = confirm("do you want password to include Lower Case?")
     if (includeAlphaLower) {
         charPool = charPool.concat(alphaLower);
     }
-
-    var includeNumber = confirm("do you want password to include any Numbers?")
     if (includeNumber) {
         charPool = charPool.concat(number);
     }
 
-    for (let i = 0; i < passwordLength; i++) {
-        var randomNum = Math.floor(Math.random() * charPool.length);
-        password = password + charPool[randomNum];
+
+    for (let i = 0; i <= passwordLength; i++) {
+        // If in the event that the math.random pulled a 1, like in this example where the user choose lowerChars&&numbers: i=[35], but the charPool.lenght in that example is = 36, and if the loop just so happened to try and grab the non excistant 36th index there would be an error. SO I added the -1 and the end of line 55.
+        var randomNum = Math.floor(Math.random() * charPool.length -1);
+        password += charPool[randomNum];
     }
     return password;
 }
